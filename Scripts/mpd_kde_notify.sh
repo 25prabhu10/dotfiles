0b912e9a5850e646eebd4c7b2f2d03813d06fe80
Prabhu K H <25prabhu10@gmail.com>
2021-07-13 00:03:06 +0530

    feat: add kdialog to show mpd current status

---
+++ Scripts/mpd_kde_notify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+# Announce current song playing in mpd in a KDE notification.
+
+# Icon support for passivepopup is implemented in more recent versions of KDE.
+mpd_music_folder_path_=$(mpc -f %file% | head -n1 | sed 's/\(.*\)\/.*\..\+$/\/run\/media\/prabhu\/Vinayak\/Music\/\1/')
+mpd_music_icon_file_=$(find $mpd_music_folder_path_ -type f -iregex '.*\.\(png\|jpg\|jpeg\|gif\|bmp\)$' -print | head -n1)
+mpd_music_notif_=$(mpc current --format "%title%")
+
+kdialog --icon "$mpd_music_icon_file_" --title "$mpd_music_notif_" --passivepopup "" 3
