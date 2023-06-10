80c27e378ac2bf56d1ee2cb47978c1a1af69473f
Prabhu K H <25prabhu10@gmail.com>
2023-06-10 13:00:09 +0530

    chore:[scripts] format mpd script file

--- Scripts/mpd_kde_notify.sh
+++ Scripts/mpd_kde_notify.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/zsh
+#!/usr/bin/env bash
 
 # announce current song playing in mpd in a KDE notification
 
 # icon support for passivepopup is implemented in more recent versions of KDE
 mpd_music_folder_path_=$(mpc -f %file% | head -n1 | sed 's/\(.*\)\/.*\..\+$/\/run\/media\/prabhu\/Vinayak\/Music\/\1/')
-mpd_music_icon_file_=$(find $mpd_music_folder_path_ -type f -iregex '.*\.\(png\|jpg\|jpeg\|gif\|bmp\)$' -print | head -n1)
+mpd_music_icon_file_=$(find "$mpd_music_folder_path_" -type f -iregex '.*\.\(png\|jpg\|jpeg\|gif\|bmp\)$' -print | head -n1)
 mpd_music_notif_=$(mpc current --format "%title%")
 
 kdialog --icon "$mpd_music_icon_file_" --title "$mpd_music_notif_" --passivepopup "" 3
