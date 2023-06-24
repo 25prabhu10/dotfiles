e355b0d48c88021c9463f219450ec10d7e577c5a
Prabhu K H <25prabhu10@gmail.com>
2023-06-24 22:15:51 +0530

    chore:[scripts] update notification method

--- Scripts/mpd_kde_notify.sh
+++ Scripts/mpd_kde_notify.sh
@@ -7,4 +7,4 @@ mpd_music_folder_path_=$(mpc -f %file% | head -n1 | sed 's/\(.*\)\/.*\..\+$/\/ru
 mpd_music_icon_file_=$(find "$mpd_music_folder_path_" -type f -iregex '.*\.\(png\|jpg\|jpeg\|gif\|bmp\)$' -print | head -n1)
 mpd_music_notif_=$(mpc current --format "%title%")
 
-kdialog --icon "$mpd_music_icon_file_" --title "$mpd_music_notif_" --passivepopup "" 3
+notify-send -i "$mpd_music_icon_file_" "$mpd_music_notif_" -a "mpd" -t 3000
